d7b7ee9b5dad6c8fda350586d2ec04d8305199c2
RevaN <abcd@vk.com>
2022-07-12 20:39:24 +0300

    create file

---
+++ src/lesson_1/lesson1.java
@@ -0,0 +1,19 @@
+package lesson_1;
+
+import java.util.Arrays;
+
+public class lesson1 {
+    public static void main(String[] args) {
+        char [] arrayChar = new char [5];
+        arrayChar [0] = 'В';
+        arrayChar [1] = 'о';
+        arrayChar [2] = 'д';
+        arrayChar [3] = 'к';
+        arrayChar [4] = 'а';
+        System.out.println(arrayChar);
+
+
+
+
+    }
+}
